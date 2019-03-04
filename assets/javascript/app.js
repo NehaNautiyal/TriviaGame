8e79fceda8ca262f454dd7048f86b99140f35636
Neha Nautiyal <nehahaha@gmail.com>
2019-03-04 13:02:50 -0500

    style changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+var chemistryQAs = {
+    question1: {
+        Question: "What is the common name for sodium chloride?",
+        Answer: "Table Salt",
+        Choice1: "Baking Soda",
+        Choice2: "Baking Powder",
+        Choice3: "Table Sugar"
+    },
+    question2: {
+        Question: "What is the common name for sodium bicarbonate?",
+        Answer: "Table Salt",
+        Choices: ["Baking Soda", "Baking Powder", "Table Sugar"]
+        }  
+    }
+
+
+//When Start button is pressed
+$("#start").on("click", function() {
+//countdown is going to appear
+//countdown is going to start decrementing
+//question1 is going to appear 
+//answer choices will appear, among them, the correct answer choice
+
+
+
+});
+
+
+
+
+
+});
